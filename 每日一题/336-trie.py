59649a4beca84f4c3a2233c0d7478741f46a113f
wchen928 <lchen.msc2019@ivey.ca>
2021-02-28 08:04:22 -0800

    styling

--- 每日一题/336-trie.py
+++ 每日一题/336-trie.py
@@ -12,20 +12,20 @@ class Solution:
         for i, word in enumerate(words):
             word = word[::-1]
             cur_level = trie
-            
+
             # mark if the remainder is a palindrome
             for j, c in enumerate(word):
                 if word[j:] == word[j:][::-1]:
                     cur_level.palindrome_suffixes.append(i)
-                    
+
                 cur_level = cur_level.next[c]
-            cur_level.ending_word = i        
-        
-        
+            cur_level.ending_word = i
+
+
         solutions = []
         for i, word in enumerate(words):
             cur_level = trie
-            
+
             for j, c in enumerate(word):
                 # case 3: CAT SOLOS - TAC
                 # found that there's a word matches
@@ -33,20 +33,21 @@ class Solution:
                     # and remainder is a palindrome
                     if word[j:] == word[j:][::-1]:
                         solutions.append([i, cur_level.ending_word])
-                        
+
                 if c not in cur_level.next:
                     break
                 cur_level = cur_level.next[c]
-            
+
             else:
                 # case 1: CAT - TAC
                 # if we found the reversed version, insert the pair
-                if cur_level.ending_word != -1 and cur_level.ending_word != i:
+                if cur_level.ending_word != -1 \
+                    and cur_level.ending_word != i:
                     solutions.append([i, cur_level.ending_word])
-                
+
                 # case 2: CAT - SOLOS TAC
                 # already found TAC/CAT and what's left is only palindrome
                 for j in cur_level.palindrome_suffixes:
                     solutions.append([i, j])
-            
+
         return solutions
