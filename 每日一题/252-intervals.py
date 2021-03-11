e0895976826dbf17331f32500688bdca8e158de1
wchen928 <lchen.msc2019@ivey.ca>
2021-03-10 20:50:06 -0800

    3/9/2021

--- 每日一题/252-intervals.py
+++ 每日一题/252-intervals.py
@@ -1,12 +1,31 @@
 class Solution:
+    # template version
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        d = collections.defaultdict(int)
+        res = 0
+        
+        for s, e in intervals:
+            d[s] += 1
+            d[e] -= 1
+        
+        for time in sorted(d.keys()):
+            res += d[time]
+            
+            if res > 1:
+                return False
+        
+        return True
+
+    """
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
         if len(intervals) < 2:
             return True
-        
+
         intervals.sort(key=lambda x: x[0])
         
         for i in range(1, len(intervals)):
             if intervals[i][0] < intervals[i - 1][1]:
                 return False
-            
+
         return True
+    """
