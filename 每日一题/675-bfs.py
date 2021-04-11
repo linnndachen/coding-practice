d2e8786179eefe5c84bf81deeb3b53cd211e76f8
wchen928 <lchen.msc2019@ivey.ca>
2021-04-11 09:40:25 -0700

    4/11/2021

--- 每日一题/675-bfs.py
+++ 每日一题/675-bfs.py
@@ -1,12 +1,14 @@
 class Solution:
     def cutOffTree(self, forest: List[List[int]]) -> int:
+        # Time - O(mn) * O(mn)
+        # Space - (Omn)
 
         trees = sorted((v, r, c) for r, row in enumerate(forest)
                        for c, v in enumerate(row) if v > 1)
 
         sr = sc = ans = 0 # source row, source col
         for _, tr, tc in trees: # target row, target col
-            d = self.bfs(forest, sr, sc, tr, tc)
+            d = self.astar(forest, sr, sc, tr, tc)
             if d < 0: 
                 return -1
             ans += d
@@ -14,6 +16,23 @@ class Solution:
 
         return ans
 
+    def astar(self, forest, sr, sc, tr, tc):
+        m, n = len(forest), len(forest[0])
+        heap = [(0, 0, sr, sc)] # f:actual dis + guess dis, g:actual dis
+        cost = {(sr, sc): 0}
+        while heap:
+            f, g, r, c = heapq.heappop(heap)
+            if r == tr and c == tc: 
+                return g
+            for nr, nc in ((r-1,c), (r+1,c), (r,c-1), (r,c+1)):
+                if 0 <= nr < m and 0 <= nc < n and forest[nr][nc]:
+                    ncost = g + 1 + abs(nr - tr) + abs(nc - tc)
+                    if ncost < cost.get((nr, nc), 9999):
+                        cost[nr, nc] = ncost
+                        heapq.heappush(heap, (ncost, g+1, nr, nc))
+        return -1
+    
+    """
     def bfs(self, forest, sr, sc, tr, tc):
         m, n = len(forest), len(forest[0])
 
@@ -34,3 +53,4 @@ class Solution:
                     queue.append((x, y, steps+1))
 
         return -1
+        """
