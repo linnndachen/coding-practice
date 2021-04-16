108e2642fda2bd94f8b9edf4b02020adf406a58d
wchen928 <lchen.msc2019@ivey.ca>
2021-04-15 18:18:13 -0700

    4/15/2021

--- 每日一题/46-backtrack.py
+++ 每日一题/46-backtrack.py
@@ -1,11 +1,28 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         res = []
-        self.dfs(nums, [], res)
+        used = [False] * len(nums)
+        self.backtrack(nums, res, [], used)
         return res
 
+    def backtrack(self, nums, res, path, used):
+        if len(path) == len(nums):
+            res.append(path[:])
+            return
+
+        for i in range(len(nums)):
+            if used[i]:
+                continue
+            used[i] = True
+            path.append(nums[i])
+            self.backtrack(nums, res, path, used)
+
+            path.pop()
+            used[i] = False
+
+    """
     def dfs(self, options, path, res):
-        # base case: when there's no nums to choose from
+        # base case: when there's no options/nums to choose from
         if not options:
             res.append(path)
 
@@ -14,3 +31,4 @@ class Solution:
             # options are anything expect [i] itself
             # path + the current [i]
             self.dfs(options[:i] + options[i+1:], path+[options[i]], res)
+    """
