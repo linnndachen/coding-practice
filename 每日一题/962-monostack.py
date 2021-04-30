9701242df71c2edf305aceca635c138a7b80b971
wchen928 <lchen.msc2019@ivey.ca>
2021-04-29 17:27:13 -0700

    4/29/2021

---
+++ 每日一题/962-monostack.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxWidthRamp(self, A: List[int]) -> int:
+        # store the index of small numbers
+        stack = []
+
+        for i, val in enumerate(A):
+            if not stack or val < A[stack[-1]]:
+                stack.append(i)      
+        maxWidth = 0
+
+        for i in range(len(A)-1, -1, -1):
+            val = A[i]
+            # if val > than the smallest num in the stack
+            while stack and val >= A[stack[-1]]:
+                maxWidth = max(maxWidth, i - stack.pop())
+
+        return maxWidth
