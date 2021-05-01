a4392fb4ad3b685df0bc08845fac7a68a6248f93
wchen928 <lchen.msc2019@ivey.ca>
2021-04-30 17:21:32 -0700

    4/30/2021

--- 每日一题/155-stack.py
+++ 每日一题/155-stack.py
@@ -1,9 +1,40 @@
 class MinStack:
-
     def __init__(self):
         """
-        initialize your data structure here.
+        # initialize your data structure here.
         """
+        # stores the delta of time - cur_mini
+        self.stack = []
+        self.mini = float('inf')
+
+    def push(self, val: int) -> None:
+        if not self.stack:
+            self.mini = val
+
+        delta = val - self.mini
+        self.stack.append(delta)
+        
+        if delta < 0:
+            self.mini = val
+        
+    def pop(self) -> None:
+        # revert the new mini to old mini
+        if self.stack[-1] < 0:
+            self.mini = self.mini - self.stack[-1]
+
+        return self.stack.pop()
+
+    def top(self) -> int:
+        if self.stack[-1] > 0:
+            return self.stack[-1] + self.mini
+        else:
+            return self.mini
+
+    def getMin(self) -> int:
+        return self.mini
+    """
+    # 低配
+    def __init__(self):
         self.stack = []
         self.mini = []
 
@@ -23,6 +54,7 @@ class MinStack:
 
     def getMin(self) -> int:
         return self.mini[-1]
+    """
 
 # Your MinStack object will be instantiated and called as such:
 # obj = MinStack()
