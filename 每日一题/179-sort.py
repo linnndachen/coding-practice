2c6a26b6546e361488246433abadfc09dc75b46e
wchen928 <lchen.msc2019@ivey.ca>
2021-05-11 12:42:04 -0700

    5/11/2021

---
+++ 每日一题/179-sort.py
@@ -0,0 +1,16 @@
+from functools import cmp_to_key
+from typing import List
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        def cmp(x, y):
+            if y + x > x + y:
+                return 1
+            elif y + x < x + y:
+                return -1
+            else:
+                return 0
+        nums = [str(num) for num in nums]
+
+        res = "".join(sorted(nums, key=cmp_to_key(cmp))).lstrip('0')
+
+        return res if res else "0"
