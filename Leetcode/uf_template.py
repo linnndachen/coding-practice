0ea09050cbe7ac52c3d8ac3e64cdd629ec1f8019
wchen928 <lchen.msc2019@ivey.ca>
2021-05-12 20:05:39 -0700

    optimized by rank

--- Leetcode/uf_template.py
+++ Leetcode/uf_template.py
@@ -26,3 +26,49 @@ class UnionFind(object):
         small, big = sorted([root_p, root_q], key=lambda x: self.sizes[x])
         self.parents[small] = big
         self.sizes[big] += self.sizes[small]
+
+class UnionFindrank:
+    # time - M + N ? 
+    # Space: N
+    def __init__(self, grid):
+        self.parents = {}
+        self.rank = {}
+        row, col = len(grid), len(grid[0])
+        self.counts = sum(grid[i][j] == "1" for i in range(row) for j in range(col))
+
+    def find(self, p):
+        while p != self.parents[p]:
+            self.parents[p] = self.find(self.parents[p])
+            p = self.parents[p]
+
+        return p
+
+
+    def add(self, p):
+        if p not in self.parents:
+            self.parents[p] = p
+            self.rank[p] = 1
+
+    def union(self, p, q):
+        # return parent node
+        i, j = self.find(p), self.find(q)
+
+        if i != j:
+            if self.rank[i] > self.rank[j]:
+                self.parents[j] = i
+            elif self.rank[i] < self.rank[j]:
+                self.parents[i] = j
+            else:
+                self.parents[i] = j
+                self.rank[j] += 1
+
+            self.counts -= 1
+
+    def get_counts(self):
+        return self.counts
+
+
+"""
+Rank is not the actual depth of the tree rather it is an upper bound. As such, on a find operation, the rank is allowed to get out of sync with the depth.
+"""
+
