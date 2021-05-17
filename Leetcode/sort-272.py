fce66ae5a9d234280cf247e510d5a0fdfb7f4ef4
wchen928 <lchen.msc2019@ivey.ca>
2021-05-17 09:47:01 -0700

    5/17/2021

--- Leetcode/sort-272.py
+++ Leetcode/sort-272.py
@@ -68,3 +68,32 @@ class Solution:
         inorder(root)
         return [x for _, x in heap]
     """
+
+    """
+    # bucket sort - wrong answer
+    # we cannot use bucket sort here because we are detailed to float nums
+    # while buckets are divided by whole number
+    # Therefore, when we have numers like 2, 3 compare with 2.571429. 
+    # We will have the wrong answer
+    def closestKValues(self, root: TreeNode, target: float, k: int) -> List[int]:
+        def inorder(node):
+            return inorder(node.left) + [node.val] + inorder(node.right) if node else []
+
+        nums = inorder(root)
+        dist = lambda idx: abs(nums[idx] - target)
+
+        counter = collections.defaultdict(list)
+        maxdist = 0
+        for idx, val in enumerate(nums):
+            diff = dist(idx)
+            counter[diff].append(val)
+            maxdist = max(maxdist, diff)
+
+        bucket = [[] for _ in range(int(maxdist) + 1)]
+        for dist, n in counter.items():
+            bucket[int(dist)].extend(n)
+
+        flat_list = [item for sublist in bucket for item in sublist]
+
+        return flat_list[:k]
+    """
