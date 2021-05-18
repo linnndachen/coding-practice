9261699850283ae77d2028096fa9c3db8b93fb60
wchen928 <lchen.msc2019@ivey.ca>
2021-05-17 21:33:33 -0700

    5/17/2021

--- 每日一题/105-tree.py
+++ 每日一题/105-tree.py
@@ -17,3 +17,22 @@ class Solution:
         root.right = self.buildTree(preorder, inorder[inorder_idx + 1:])
 
         return root
+
+    # optimized
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+        inorder_map = {value: index for index, value in enumerate(inorder)}
+        pre_iter = iter(preorder)
+
+        
+        def _helper(start, end):
+            if start > end:
+                return None
+            root_val = next(pre_iter)
+            root = TreeNode(root_val)
+            idx = inorder_map[root_val]
+            
+            root.left = _helper(start, idx-1)
+            root.right = _helper(idx+1, end)
+            return root
+        
+        return _helper(0, len(inorder) - 1)
