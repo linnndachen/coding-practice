252a9aac60c853bd2ac8e15a5854f7a776593cf6
wchen928 <lchen.msc2019@ivey.ca>
2021-06-13 12:16:23 -0700

    6/13/2021

--- Leetcode/tree-297.py
+++ Leetcode/tree-297.py
@@ -1,56 +1,69 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+from collections import deque
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 class Codec:
-
+    """
+    # bfs
     def serialize(self, root):
-        """Encodes a tree to a single string.
-        
-        :type root: TreeNode
-        :rtype: str
-        """
         if not root:
-            return 'x'
-        
-        # return (1, (2, 'x', 'x'), (3, (4, 'x', 'x'), (5, 'x', 'x')))
-        return root.val, self.serialize(root.left), self.serialize(root.right)
-        
-        # string
-        # return ','.join([str(root.val), self.serialize(root.left), self.serialize(root.right)])
-        
-        
+            return ''
+        queue = collections.deque([root])
+        res = ''
+        while queue:
+            node = queue.popleft()
+            if not node:
+                res += 'None,'
+                continue
+            res += str(node.val) + ','
+            queue.append(node.left)
+            queue.append(node.right)
+        return res
 
     def deserialize(self, data):
-        """Decodes your encoded data to tree.
-        
-        :type data: str
-        :rtype: TreeNode
-        """
-        if data[0] == 'x': 
+        if not data:
             return None
-        node = TreeNode(data[0])
-        node.left = self.deserialize(data[1])
-        node.right = self.deserialize(data[2])
-        return node
-    
+        ls = data.split(',')
+
+        root = TreeNode(int(ls[0]))
+        queue = collections.deque([root])
+        i = 1
+        while queue and i < len(ls):
+            node = queue.popleft()
+            if ls[i] != 'None':
+                left = TreeNode(int(ls[i]))
+                node.left = left
+                queue.append(left)
+            i += 1
+            if ls[i] != 'None':
+                right = TreeNode(int(ls[i]))
+                node.right = right
+                queue.append(right)
+            i += 1
+        return root
     """
-        def deserialize(self, data):
-        # string
-		# data stream will be consumed  as we build left side of Tree
-		# by the time when the right side is building up, we need to 
-        # hold what is left over.
-		# Therefore, self.data is a global value, right side will use 
-        # what is left over after tree is partially built
-        self.data = data
-        if self.data[0] == 'x': return None
-        node = TreeNode(self.data[:self.data.find(',')]) 
-        node.left = self.deserialize(self.data[self.data.find(',')+1:])
-        node.right = self.deserialize(self.data[self.data.find(',')+1:])
-        return node
-    
-    """
-        
+
+    def serialize(self, root):
+        if not root:
+            return "x"
+
+        return str(root.val) + "," + self.serialize(root.left) + "," + self.serialize(root.right)
+
+    def deserialize(self, data):
+        def helper(queue):
+            root_val = queue.popleft()
+            if root_val == "x":
+                return None
+
+            root = TreeNode(int(root_val))
+            root.left = helper(queue)
+            root.right = helper(queue)
+            return root
+
+        lst = deque(data.split(','))
+
+        return helper(lst)
