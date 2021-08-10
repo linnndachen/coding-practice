4ddfa966b5c85358415f9e7c52e0d07104795720
wchen928 <lchen.msc2019@ivey.ca>
2021-08-10 11:30:10 -0700

    8/10/2021

--- Leetcode/077-dfs.py
+++ Leetcode/077-dfs.py
@@ -1,3 +1,5 @@
+from typing import List
+
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         res = []
@@ -13,3 +15,25 @@ class Solution:
             path.append(num)
             self.backtrack(n, k, path, num+1, res)
             path.pop()
+
+class Solution2:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        # init first combination
+        # the[n+1] is a sentinel
+        nums = list(range(1, k + 1)) + [n + 1]
+
+        output, j = [], 0
+        while j < k:
+            output.append(nums[:k])
+            # increase first nums[j] by one
+            # if nums[j] + 1 != nums[j + 1]
+            j = 0
+            # if left and right pointer right next to each other
+            while j < k and nums[j + 1] == nums[j] + 1:
+                # move the right pointer
+                nums[j] = j + 1
+                j += 1
+            # else, move the left pointer
+            nums[j] += 1
+            
+        return output
