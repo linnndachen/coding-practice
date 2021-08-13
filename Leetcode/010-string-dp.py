5bfc591bff15b637d29fe98372584f34845d665e
wchen928 <lchen.msc2019@ivey.ca>
2021-08-12 20:56:38 -0700

    8/12/2021

--- Leetcode/010-string-dp.py
+++ Leetcode/010-string-dp.py
@@ -1,3 +1,35 @@
+class Solution1:
+    def isMatch(self, s: str, p: str) -> bool:
+        s = "#"+s
+        p = "#"+p
+
+        m, n = len(s), len(p)
+        dp = [[0]*n for _ in range(m)]
+
+        dp[0][0] = 1
+
+        for j in range(2, n):
+            dp[0][j] = (p[j] == "*" and dp[0][j-2])
+
+        for i in range(1, m):
+            for j in range(1, n):
+                if s[i] == p[j] or p[j] == ".":
+                    dp[i][j] = dp[i-1][j-1]
+
+                elif p[j] == "*":
+                    # i-1 needs to match with j because j is a repeat of j-1
+                    # and j can represent many j-1
+                    opt1 = dp[i-1][j] and (p[j-1] == s[i] or p[j-1] == ".")
+                    # when * represents 0, cancel 2 chars
+                    opt2 = dp[i][j-2]
+
+                    dp[i][j] = opt1 or opt2
+
+        return dp[m-1][n-1]
+
+
+
+
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         if p == ".*":
