13c0f94d97c1c89780063fb223fc531e5d8c9535
wchen928 <lchen.msc2019@ivey.ca>
2021-08-23 09:18:52 -0700

    8/17/2021

--- Leetcode_By_Topic/bit_math-338.py
+++ Leetcode_By_Topic/bit_math-338.py
@@ -1,10 +1,18 @@
 from typing import List
+
 class Solution:
     def countBits(self, n: int) -> List[int]:
         res = [0] * (n+1)
 
         for i in range(1, n+1):
-            # take the divide by 2 value and + if it was odd / even
-            res[i] = res[i>>1] + (i&1)
+            # countBits(all bits of num except the last bit) 
+            # + countBits(the last bit)
 
+            # if it was a even num - ending in 0 bit
+            if (i&1) == 0:
+                # the last bit can be ignore
+                res[i] = res[i>>1]
+            else:
+                # if it was odd, the previous 1's we have + the new one
+                res[i] = res[i>>1] + 1
         return res
